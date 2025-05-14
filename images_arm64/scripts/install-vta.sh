db860cb51175a05c8fd9c6bbde30d25f78a9795f
Jonas Kaufmann <jokau03@gmail.com>
2025-05-14 15:37:42 +0200

    images_arm64/install-vta.sh: explicitly set CMAKE_BUILD_TYPE for TVM

--- images_arm64/scripts/install-vta.sh
+++ images_arm64/scripts/install-vta.sh
@@ -11,6 +11,7 @@ cp cmake/config.cmake build
 cd build
 echo "set(USE_LLVM OFF)" >> config.cmake
 echo "set(SUMMARIZE ON)" >> config.cmake
+echo "set(CMAKE_BUILD_TYPE RelWithDebInfo)" >> config.cmake
 export CXXFLAGS="-D SIM_CTRL=1"
 cmake ..
 make -j`nproc` runtime vta
