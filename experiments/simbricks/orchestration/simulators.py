6d9fe4e2939cc726f9974cad6872609cdee52397
Jonas Kaufmann <jkaufman@mpi-sws.org>
2025-11-18 15:53:17 +0000

    orchestration/simulators::HierVtaVerilatorDev: remove legacy code for compiling VTA on-demand

--- experiments/simbricks/orchestration/simulators.py
+++ experiments/simbricks/orchestration/simulators.py
@@ -1300,7 +1300,6 @@ class HierVtaVerilatorDev(PCIDevSim):
         self.sampling_len_ns = sampling_len_ns
 
     def run_cmd(self, env: ExpEnv) -> str:
-        # script_path = self._write_bash_script(env)
         bin = f"{env.repodir}/sims/external/vta/vta_synth_sim_1x16_{self.clock_freq}_{self.trace_mode.to_full_str()}"
         workdir = f"{env.workdir}/{self.full_name()}"
         trace_file = f"{workdir}/verilator_trace"
@@ -1318,54 +1317,6 @@ class HierVtaVerilatorDev(PCIDevSim):
         args_quoted = [shlex.quote(arg) for arg in args]
         args_all = " ".join(args_quoted)
         return f"{bin} {args_all}"
-
-    def _write_bash_script(self, env: ExpEnv) -> str:
-        workdir = f"{env.workdir}/{self.full_name()}"
-        os.makedirs(workdir, exist_ok=True)
-        verilator_src_dir = f"{env.repodir}/sims/external/hier_vta_synth"
-        verilator_build_dir = f"{workdir}/build"
-        verilator_bin = f"{verilator_build_dir}/vta_sim_1x16_{self.clock_freq}"
-
-        lines = [
-            "#!/bin/bash",
-            "set -e",
-        ]
-
-        # add verilator flags
-        vflags = [
-            f'-GSIMBRICKS_PCI_SOCKET=\\"{env.dev_pci_path(self)}\\"',
-            f'-GSHM_PATH=\\"{env.dev_shm_path(self)}\\"',
-            f"-GSYNC_PERIOD={self.sync_period}",
-            f"-GCLK_FREQ_MHZ={self.clock_freq}",
-            f"-GPCI_LATENCY={self.pci_latency}",
-        ]
-        if self.trace_mode == 1:
-            vflags.extend(["--trace-vcd", "--no-trace-top", "--trace-depth 2"])
-        elif self.trace_mode == 2:
-            vflags.extend(["--trace-saif", "--no-trace-top"])
-        vflags_str = shlex.quote(" ".join(vflags))
-        lines.append(f"export ADDITIONAL_VFLAGS={vflags_str}")
-
-        # add C flags
-        cflags = [f"-DTRACE_MODE={self.trace_mode}"]
-        cflags_str = shlex.quote(" ".join(cflags))
-        lines.append(f"export ADDITIONAL_CFLAGS={cflags_str}")
-
-        # copy source directory to workdir and build
-        lines.append(f"cp -r {verilator_src_dir} {verilator_build_dir}")
-        lines.append(f"cd {verilator_build_dir}")
-        lines.append("source .envrc")
-        lines.append(f"make vta_sim_1x16_{self.clock_freq}")
-
-        trace_file = f"{workdir}/verilator_trace"
-        lines.append(
-            f"{verilator_bin} {self.clock_freq} {trace_file} {self.sampling_period_ns} {self.sampling_len_ns}"
-        )
-
-        script_path = f"{workdir}/compile_and_run.sh"
-        with open(script_path, mode="w", encoding="utf-8") as file:
-            file.writelines([f"{line}\n" for line in lines])
-        return f"setsid --wait bash {script_path}"
 
 
 class BasicMemDev(MemDevSim):
