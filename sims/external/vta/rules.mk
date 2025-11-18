bf2b447fa8c5a04b1e7ece50c0f9fb7c9c288bba
Jonas Kaufmann <jkaufman@mpi-sws.org>
2025-11-18 14:56:00 +0000

    sims/external/vta: add vta_rtl_sim_1x16_no_trace

--- sims/external/vta/rules.mk
+++ sims/external/vta/rules.mk
@@ -16,10 +16,18 @@ VCD_TRACE_VFLAGS := $(VFLAGS) $(ADDITIONAL_VFLAGS) --trace-vcd --no-trace-top --
 SAIF_TRACE_CFLAGS := $(CFLAGS) $(ADDITIONAL_CFLAGS) -DTRACE_MODE=2
 SAIF_TRACE_VFLAGS := $(VFLAGS) $(ADDITIONAL_VFLAGS) --trace-saif --no-trace-top
 
-BIN := $(d)vta_synth_sim_1x16_100_no_trace $(d)vta_synth_sim_1x16_174_no_trace $(d)vta_synth_sim_1x16_100_vcd_trace $(d)vta_synth_sim_1x16_174_vcd_trace $(d)vta_synth_sim_1x16_100_saif_trace $(d)vta_synth_sim_1x16_174_saif_trace
+BIN := $(d)vta_synth_sim_1x16_100_no_trace \
+	$(d)vta_synth_sim_1x16_174_no_trace \
+	$(d)vta_synth_sim_1x16_100_vcd_trace \
+	$(d)vta_synth_sim_1x16_174_vcd_trace \
+	$(d)vta_synth_sim_1x16_100_saif_trace \
+	$(d)vta_synth_sim_1x16_174_saif_trace
 VERILATOR_XILINX := $(d)verilator_xilinx
 
 vta-all: $(BIN)
+
+$(d)vta_rtl_sim_1x16_no_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x16.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
+	verilator --top-module vta_sim -CFLAGS "$(NO_TRACE_CFLAGS)" -y $(VERILATOR_XILINX) $(NO_TRACE_VFLAGS) --binary --build --exe -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_100_no_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_100.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
 	verilator --top-module vta_sim -CFLAGS "$(NO_TRACE_CFLAGS)" -y $(VERILATOR_XILINX) $(NO_TRACE_VFLAGS) --binary --build --exe -o $(abspath $@) $^
