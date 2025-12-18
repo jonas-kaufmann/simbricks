4465533c99a76ab5641ce152c0293ed87bcc33be
Jonas Kaufmann <jkaufman@mpi-sws.org>
2025-12-18 15:43:44 +0000

    images_arm64/scripts/install-vta.sh: use ARM Compute Library

--- images_arm64/scripts/install-vta.sh
+++ images_arm64/scripts/install-vta.sh
@@ -5,11 +5,17 @@ set -eux
 mkdir -p /root
 git clone --depth 1 --recursive --branch acdsim-v0.8.0 https://github.com/jonas-kaufmann/tvm-simbricks.git /root/tvm
 cd /root/tvm
+
+# install ARM Compute Libary
+bash docker/install/ubuntu_download_arm_compute_lib_binaries.sh
+
+# build TVM
 cp 3rdparty/vta-hw/config/simbricks_pci_sample.json 3rdparty/vta-hw/config/vta_config.json
 mkdir build
 cp cmake/config.cmake build
 cd build
 echo "set(USE_LLVM OFF)" >> config.cmake
+echo "set(USE_ARM_COMPUTE_LIB_GRAPH_EXECUTOR ON)" >> config.cmake
 echo "set(SUMMARIZE ON)" >> config.cmake
 echo "set(CMAKE_BUILD_TYPE RelWithDebInfo)" >> config.cmake
 export CXXFLAGS="-D SIM_CTRL=1"
