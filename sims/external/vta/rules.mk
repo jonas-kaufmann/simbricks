71c4eba6f391428720f67a2da70d4e355b3294e2
Jonas Kaufmann <jkaufman@mpi-sws.org>
2026-02-05 16:59:20 +0000

    sims/external/vta/rules.mk: replace --binary in Verilator invocation with --cc
    Somehow, --binary lead to two main() functions being produced.

--- sims/external/vta/rules.mk
+++ sims/external/vta/rules.mk
@@ -5,7 +5,7 @@ VERILATOR_MDIR := $(d)obj_dir
 ADDITIONAL_CFLAGS ?=
 ADDITIONAL_VFLAGS ?=
 BASE_CFLAGS := -I$(abspath $(lib_dir)) -std=c++17 $(ADDITIONAL_CFLAGS)
-BASE_VFLAGS := -Wno-fatal --threads 2 -j `nproc` -O3 --compiler clang -MAKEFLAGS "OPT=-march=native" --Mdir $(VERILATOR_MDIR)
+BASE_VFLAGS := -Wno-fatal --threads 2 -j `nproc` -O3 --compiler clang -MAKEFLAGS "OPT=-march=native" --Mdir $(VERILATOR_MDIR) --cc --build --exe
 
 NO_TRACE_CFLAGS := $(BASE_CFLAGS) $(ADDITIONAL_CFLAGS) -DTRACE_MODE=0
 NO_TRACE_VFLAGS := $(BASE_VFLAGS) $(ADDITIONAL_VFLAGS)
@@ -35,84 +35,84 @@ $(d)vta_rtl_sim_1x16_no_trace: private VFLAGS := $(NO_TRACE_VFLAGS)
 $(d)vta_rtl_sim_1x16_no_trace: CC := clang-20
 $(d)vta_rtl_sim_1x16_no_trace: CXX := clang++-20
 $(d)vta_rtl_sim_1x16_no_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x16.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_rtl_sim_1x16_vcd_trace: private CFLAGS := $(VCD_TRACE_CFLAGS)
 $(d)vta_rtl_sim_1x16_vcd_trace: private VFLAGS := $(VCD_TRACE_VFLAGS)
 $(d)vta_rtl_sim_1x16_vcd_trace: CC := clang-20
 $(d)vta_rtl_sim_1x16_vcd_trace: CXX := clang++-20
 $(d)vta_rtl_sim_1x16_vcd_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x16.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_rtl_sim_1x16_saif_trace: private CFLAGS := $(SAIF_TRACE_CFLAGS)
 $(d)vta_rtl_sim_1x16_saif_trace: private VFLAGS := $(SAIF_TRACE_VFLAGS)
 $(d)vta_rtl_sim_1x16_saif_trace: CC := clang-20
 $(d)vta_rtl_sim_1x16_saif_trace: CXX := clang++-20
 $(d)vta_rtl_sim_1x16_saif_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x16.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_rtl_sim_1x32_no_trace: private CFLAGS := $(NO_TRACE_CFLAGS)
 $(d)vta_rtl_sim_1x32_no_trace: private VFLAGS := $(NO_TRACE_VFLAGS)
 $(d)vta_rtl_sim_1x32_no_trace: CC := clang-20
 $(d)vta_rtl_sim_1x32_no_trace: CXX := clang++-20
 $(d)vta_rtl_sim_1x32_no_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x32.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_rtl_sim_1x32_vcd_trace: private CFLAGS := $(VCD_TRACE_CFLAGS)
 $(d)vta_rtl_sim_1x32_vcd_trace: private VFLAGS := $(VCD_TRACE_VFLAGS)
 $(d)vta_rtl_sim_1x32_vcd_trace: CC := clang-20
 $(d)vta_rtl_sim_1x32_vcd_trace: CXX := clang++-20
 $(d)vta_rtl_sim_1x32_vcd_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x32.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_rtl_sim_1x32_saif_trace: private CFLAGS := $(SAIF_TRACE_CFLAGS)
 $(d)vta_rtl_sim_1x32_saif_trace: private VFLAGS := $(SAIF_TRACE_VFLAGS)
 $(d)vta_rtl_sim_1x32_saif_trace: CC := clang-20
 $(d)vta_rtl_sim_1x32_saif_trace: CXX := clang++-20
 $(d)vta_rtl_sim_1x32_saif_trace: $(d)vta_sim_rtl.sv $(d)vta_rtl_1x32.sv $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_100_no_trace: private CFLAGS := $(NO_TRACE_CFLAGS)
 $(d)vta_synth_sim_1x16_100_no_trace: private VFLAGS := $(NO_TRACE_VFLAGS)
 $(d)vta_synth_sim_1x16_100_no_trace: CC := clang-20
 $(d)vta_synth_sim_1x16_100_no_trace: CXX := clang++-20
 $(d)vta_synth_sim_1x16_100_no_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_100.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_174_no_trace: private CFLAGS := $(NO_TRACE_CFLAGS)
 $(d)vta_synth_sim_1x16_174_no_trace: private VFLAGS := $(NO_TRACE_VFLAGS)
 $(d)vta_synth_sim_1x16_174_no_trace: CC := clang-20
 $(d)vta_synth_sim_1x16_174_no_trace: CXX := clang++-20
 $(d)vta_synth_sim_1x16_174_no_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_174.v $(d)m_axil_adapter.sv $(d)$(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_100_vcd_trace: private CFLAGS := $(VCD_TRACE_CFLAGS)
 $(d)vta_synth_sim_1x16_100_vcd_trace: private VFLAGS := $(VCD_TRACE_VFLAGS)
 $(d)vta_synth_sim_1x16_100_vcd_trace: CC := clang-20
 $(d)vta_synth_sim_1x16_100_vcd_trace: CXX := clang++-20
 $(d)vta_synth_sim_1x16_100_vcd_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_100.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_174_vcd_trace: private CFLAGS := $(VCD_TRACE_CFLAGS)
 $(d)vta_synth_sim_1x16_174_vcd_trace: private VFLAGS := $(VCD_TRACE_VFLAGS)
 $(d)vta_synth_sim_1x16_174_vcd_trace: CC := clang-20
 $(d)vta_synth_sim_1x16_174_vcd_trace: CXX := clang++-20
 $(d)vta_synth_sim_1x16_174_vcd_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_174.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_100_saif_trace: private CFLAGS := $(SAIF_TRACE_CFLAGS)
 $(d)vta_synth_sim_1x16_100_saif_trace: private VFLAGS := $(SAIF_TRACE_VFLAGS)
 $(d)vta_synth_sim_1x16_100_saif_trace: CC := clang-20
 $(d)vta_synth_sim_1x16_100_saif_trace: CXX := clang++-20
 $(d)vta_synth_sim_1x16_100_saif_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_100.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 $(d)vta_synth_sim_1x16_174_saif_trace: private CFLAGS := $(SAIF_TRACE_CFLAGS)
 $(d)vta_synth_sim_1x16_174_saif_trace: private VFLAGS := $(SAIF_TRACE_VFLAGS)
 $(d)vta_synth_sim_1x16_174_saif_trace: CC := clang-20
 $(d)vta_synth_sim_1x16_174_saif_trace: CXX := clang++-20
 $(d)vta_synth_sim_1x16_174_saif_trace: $(d)vta_sim_synth.sv $(d)vta_synth_1x16_174.v $(d)m_axil_adapter.sv $(d)s_axi_adapter.sv $(d)verilator_adapter.cc $(d)verilator_main.cc $(abspath $(lib_simbricks))
-	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) --binary --build --exe -o $(abspath $@) $^
+	verilator --top-module vta_sim -CFLAGS "$(CFLAGS)" -y $(VERILATOR_XILINX) $(VFLAGS) -o $(abspath $@) $^
 
 vta-clean:
 	rm -rf $(BIN) $(VERILATOR_MDIR)
