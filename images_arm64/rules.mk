26163069b7561f8f193f0e33e1bfbf636bb46f5b
Jonas Kaufmann <jkaufman@mpi-sws.org>
2026-02-07 17:54:46 +0000

    images_arm64/rules.mk: add _arm64 trailer to variable names to allow to coexist with images/rules.mk

--- images_arm64/rules.mk
+++ images_arm64/rules.mk
@@ -22,42 +22,49 @@
 
 include mk/subdir_pre.mk
 
-PACKER_VERSION := 1.7.0
-KERNEL_VERSION := 5.15.93
+PACKER_VERSION_ARM64 := 1.7.0
+KERNEL_VERSION_ARM64 := 5.15.93
 
-BASE_IMAGE := $(d)output-base/base
-VTA_DEP_IMAGE := $(d)output-vta_dep/vta_dep
-VTA_IMAGE := $(d)output-vta/vta
-GEMSTONE_IMAGE := $(d)output-gemstone/gemstone
-COMPRESSED_IMAGES ?= false
+BASE_IMAGE_ARM64 := $(d)output-base/base
+MEMCACHED_IMAGE_ARM64 := $(d)output-memcached/memcached
+NOPAXOS_IMAGE_ARM64 := $(d)output-nopaxos/nopaxos
+VTA_DEP_IMAGE_ARM64 := $(d)output-vta_dep/vta_dep
+VTA_IMAGE_ARM64 := $(d)output-vta/vta
+GEMSTONE_IMAGE_ARM64 := $(d)output-gemstone/gemstone
+COMPRESSED_IMAGES_ARM64 ?= false
 
-IMAGES := $(BASE_IMAGE) $(NOPAXOS_IMAGE) $(MEMCACHED_IMAGE) $(VTA_IMAGE)
-RAW_IMAGES := $(addsuffix .raw,$(IMAGES))
+IMAGES_ARM64 := $(BASE_IMAGE_ARM64) $(NOPAXOS_IMAGE_ARM64) \
+    $(MEMCACHED_IMAGE_ARM64) \
+    $(VTA_IMAGE_ARM64)
+RAW_IMAGES_ARM64 := $(addsuffix .raw,$(IMAGES_ARM64))
 
-IMAGES_MIN := $(BASE_IMAGE)
-RAW_IMAGES_MIN := $(addsuffix .raw,$(IMAGES_MIN))
+IMAGES_MIN_ARM64 := $(BASE_IMAGE_ARM64)
+RAW_IMAGES_MIN_ARM64 := $(addsuffix .raw,$(IMAGES_MIN_ARM64))
 
-img_dir := $(d)
-packer := $(d)packer
+img_dir_arm64 := $(d)
+packer_arm64 := $(d)packer_arm64
 
-bz_image := $(d)bzImage
-vmlinux := $(d)vmlinux
-kernel_pardir := $(d)kernel
-kernel_dir := $(kernel_pardir)/linux-$(KERNEL_VERSION)
-kernel_config := $(kernel_pardir)/config-$(KERNEL_VERSION)
-kernel_options := ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
-kheader_dir := $(d)kernel/kheaders
-kheader_tar := $(d)kheaders.tar.bz2
-m5_bin := $(d)m5
-guest_init := $(d)/scripts/guestinit.sh
+bz_image_arm64 := $(d)bzImage
+vmlinux_arm64 := $(d)vmlinux
+kernel_pardir_arm64 := $(d)kernel
+kernel_dir_arm64 := $(kernel_pardir_arm64)/linux-$(KERNEL_VERSION_ARM64)
+kernel_config_arm64 := $(kernel_pardir_arm64)/config-$(KERNEL_VERSION_ARM64)
+kernel_options_arm64 := ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+kheader_dir_arm64 := $(d)kernel/kheaders
+kheader_tar_arm64 := $(d)kheaders.tar.bz2
+m5_bin_arm64 := $(d)m5
+guest_init_arm64 := $(d)/scripts/guestinit.sh
 
-build-images: $(IMAGES) $(RAW_IMAGES) $(vmlinux) $(bz_image)
+build-images-aarch64: $(IMAGES_ARM64) $(RAW_IMAGES_ARM64) \
+    $(vmlinux_arm64) \
+    $(bz_image_arm64)
 
-build-images-min: $(IMAGES_MIN) $(RAW_IMAGES_MIN) $(vmlinux) $(bz_image)
+build-images-min-aarch64: $(IMAGES_MIN_ARM64) $(RAW_IMAGES_MIN_ARM64) \
+    $(vmlinux_arm64) $(bz_image_arm64)
 
 # only converts existing images to raw
-convert-images-raw:
-	for i in $(IMAGES); do \
+convert-images-raw-aarch64:
+	for i in $(IMAGES_ARM64); do \
 	    [ -f $$i ] || continue; \
 	    $(QEMU_IMG) convert -f qcow2 -O raw $$i $${i}.raw ; done
 
@@ -67,107 +74,110 @@ convert-images-raw:
 %.raw: %
 	$(QEMU_IMG) convert -f qcow2 -O raw $< $@
 
-$(BASE_IMAGE): $(packer) $(QEMU) $(bz_image) $(m5_bin) $(kheader_tar) \
-    $(guest_init) $(kernel_config) \
+$(BASE_IMAGE_ARM64): $(packer_arm64) $(QEMU) $(bz_image_arm64) \
+    $(m5_bin_arm64) $(kheader_tar_arm64) $(guest_init_arm64) \
+    $(kernel_config_arm64) \
     $(addprefix $(d), extended-image.pkr.hcl scripts/install-base.sh \
       scripts/cleanup.sh)
 	rm -rf $(dir $@)
-	mkdir -p $(img_dir)input-base
-	cp $(m5_bin) $(kheader_tar) $(guest_init) $(bz_image) $(kernel_config) \
-	    $(img_dir)input-base/
-	truncate -s 64m $(img_dir)varstore.img
-	truncate -s 64m $(img_dir)efi.img
-	dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of=$(img_dir)efi.img conv=notrunc
-	cd $(img_dir) && ./packer-wrap.sh base base base.pkr.hcl \
-	    $(COMPRESSED_IMAGES)
-	rm -rf $(img_dir)input-base
+	mkdir -p $(img_dir_arm64)input-base
+	cp $(m5_bin_arm64) $(kheader_tar_arm64) $(guest_init_arm64) \
+	    $(bz_image_arm64) $(kernel_config_arm64) \
+	    $(img_dir_arm64)input-base/
+	truncate -s 64m $(img_dir_arm64)varstore.img
+	truncate -s 64m $(img_dir_arm64)efi.img
+	dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of=$(img_dir_arm64)efi.img conv=notrunc
+	cd $(img_dir_arm64) && ./packer_arm64-wrap.sh base base base.pkr.hcl \
+	    $(COMPRESSED_IMAGES_ARM64)
+	rm -rf $(img_dir_arm64)input-base
 	touch $@
 
-TVM_DIR := $(img_dir)tvm
+TVM_DIR_ARM64 := $(img_dir_arm64)tvm
 
-$(VTA_DEP_IMAGE): $(packer) $(QEMU) $(BASE_IMAGE) \
+$(VTA_DEP_IMAGE_ARM64): $(packer_arm64) $(QEMU) $(BASE_IMAGE_ARM64) \
     $(addprefix $(d), extended-image.pkr.hcl scripts/install-vta_dep.sh \
       scripts/cleanup.sh)
 	rm -rf $(dir $@)
-	cd $(img_dir) && ./packer-wrap.sh base vta_dep extended-image.pkr.hcl \
-	    $(COMPRESSED_IMAGES)
+	cd $(img_dir_arm64) && ./packer_arm64-wrap.sh base vta_dep extended-image.pkr.hcl \
+	    $(COMPRESSED_IMAGES_ARM64)
 	touch $@
 
-$(VTA_IMAGE): $(packer) $(QEMU) $(VTA_DEP_IMAGE) \
+$(VTA_IMAGE_ARM64): $(packer_arm64) $(QEMU) $(VTA_DEP_IMAGE_ARM64) \
     $(addprefix $(d), extended-image.pkr.hcl scripts/install-vta.sh \
       scripts/cleanup.sh)
 	rm -rf $(dir $@)
-	cd $(img_dir) && ./packer-wrap.sh vta_dep vta extended-image.pkr.hcl \
-	    $(COMPRESSED_IMAGES)
+	cd $(img_dir_arm64) && ./packer_arm64-wrap.sh vta_dep vta extended-image.pkr.hcl \
+	    $(COMPRESSED_IMAGES_ARM64)
 	touch $@
 
-$(GEMSTONE_IMAGE): $(packer) $(QEMU) $(BASE_IMAGE) \
+$(GEMSTONE_IMAGE_ARM64): $(packer_arm64) $(QEMU) $(BASE_IMAGE_ARM64) \
     $(addprefix $(d), extended-image.pkr.hcl scripts/install-gemstone.sh \
       scripts/cleanup.sh)
 	rm -rf $(dir $@)
-	rm -rf $(img_dir)input-gemstone
-	mkdir -p $(img_dir)input-gemstone
-	ln -s /home/jonask/Repos/cpu_micro_benchmarks $(img_dir)input-gemstone/cpu_micro_benchmarks
-	cd $(img_dir) && ./packer-wrap.sh base gemstone extended-image.pkr.hcl \
-	    $(COMPRESSED_IMAGES)
+	rm -rf $(img_dir_arm64)input-gemstone
+	mkdir -p $(img_dir_arm64)input-gemstone
+	ln -s /home/jonask/Repos/cpu_micro_benchmarks $(img_dir_arm64)input-gemstone/cpu_micro_benchmarks
+	cd $(img_dir_arm64) && ./packer_arm64-wrap.sh base gemstone extended-image.pkr.hcl \
+	    $(COMPRESSED_IMAGES_ARM64)
 	touch $@
 
 
-$(packer):
-	wget -O $(img_dir)packer_$(PACKER_VERSION)_linux_amd64.zip \
-	    https://releases.hashicorp.com/packer/$(PACKER_VERSION)/packer_$(PACKER_VERSION)_linux_amd64.zip
-	cd $(img_dir) && unzip packer_$(PACKER_VERSION)_linux_amd64.zip
-	rm -f $(img_dir)packer_$(PACKER_VERSION)_linux_amd64.zip
+$(packer_arm64):
+	wget -O $(img_dir_arm64)packer_$(PACKER_VERSION_ARM64)_linux_amd64.zip \
+	    https://releases.hashicorp.com/packer_arm64/$(PACKER_VERSION_ARM64)/packer_$(PACKER_VERSION_ARM64)_linux_amd64.zip
+	cd $(img_dir_arm64) && unzip packer_$(PACKER_VERSION_ARM64)_linux_amd64.zip
+	rm -f $(img_dir_arm64)packer_$(PACKER_VERSION_ARM64)_linux_amd64.zip
 
 
 ################################################
 # Kernel
 
-$(kernel_dir)/vmlinux: $(kernel_dir)/.config
-	$(MAKE) $(kernel_options) -C $(kernel_dir)
+$(kernel_dir_arm64)/vmlinux: $(kernel_dir_arm64)/.config
+	$(MAKE) $(kernel_options_arm64) -C $(kernel_dir_arm64)
 	touch $@
 
-$(vmlinux): $(kernel_dir)/vmlinux
+$(vmlinux_arm64): $(kernel_dir_arm64)/vmlinux
 	cp $< $@
 	touch $@
 
 # this dependency is a bit stupid, but not sure how to better do this
-$(bz_image): $(kernel_dir)/vmlinux
-	cp $(kernel_dir)/arch/arm64/boot/Image.gz $@
+$(bz_image_arm64): $(kernel_dir_arm64)/vmlinux
+	cp $(kernel_dir_arm64)/arch/arm64/boot/Image.gz $@
 	touch $@
 
-$(kheader_tar): $(kernel_dir)/vmlinux
-	rm -rf $(kheader_dir)
-	mkdir -p $(kheader_dir)
-	$(MAKE) $(kernel_options) -C $(kernel_dir) headers_install INSTALL_HDR_PATH=$(abspath $(kheader_dir)/usr)
-	$(MAKE) $(kernel_options) -C $(kernel_dir) modules_install INSTALL_MOD_PATH=$(abspath $(kheader_dir))
-	rm -f $(kheader_dir)/lib/modules/$(KERNEL_VERSION)/build
-	ln -s /usr/src/linux-headers-$(KERNEL_VERSION) \
-	    $(kheader_dir)/lib/modules/$(KERNEL_VERSION)/build
-	rm -f $(kheader_dir)/lib/modules/$(KERNEL_VERSION)/source
-	mkdir -p $(kheader_dir)/usr/src/linux-headers-$(KERNEL_VERSION)
-	cp -r $(kernel_dir)/.config $(kernel_dir)/Makefile \
-	    $(kernel_dir)/Module.symvers $(kernel_dir)/scripts \
-	    $(kernel_dir)/include \
-	    $(kheader_dir)/usr/src/linux-headers-$(KERNEL_VERSION)/
-	mkdir -p $(kheader_dir)/usr/src/linux-headers-$(KERNEL_VERSION)/tools/objtool/
-	mkdir -p $(kheader_dir)/usr/src/linux-headers-$(KERNEL_VERSION)/arch/arm64/
-	cp -r $(kernel_dir)/arch/arm64/Makefile \
-	    $(kernel_dir)/arch/arm64/include \
-	    $(kheader_dir)/usr/src/linux-headers-$(KERNEL_VERSION)/arch/arm64
-	cd $(kheader_dir) && tar cjf $(abspath $@) .
+$(kheader_tar_arm64): $(kernel_dir_arm64)/vmlinux
+	rm -rf $(kheader_dir_arm64)
+	mkdir -p $(kheader_dir_arm64)
+	$(MAKE) $(kernel_options_arm64) -C $(kernel_dir_arm64) headers_install INSTALL_HDR_PATH=$(abspath $(kheader_dir_arm64)/usr)
+	$(MAKE) $(kernel_options_arm64) -C $(kernel_dir_arm64) modules_install INSTALL_MOD_PATH=$(abspath $(kheader_dir_arm64))
+	rm -f $(kheader_dir_arm64)/lib/modules/$(KERNEL_VERSION_ARM64)/build
+	ln -s /usr/src/linux-headers-$(KERNEL_VERSION_ARM64) \
+	    $(kheader_dir_arm64)/lib/modules/$(KERNEL_VERSION_ARM64)/build
+	rm -f $(kheader_dir_arm64)/lib/modules/$(KERNEL_VERSION_ARM64)/source
+	mkdir -p $(kheader_dir_arm64)/usr/src/linux-headers-$(KERNEL_VERSION_ARM64)
+	cp -r $(kernel_dir_arm64)/.config $(kernel_dir_arm64)/Makefile \
+	    $(kernel_dir_arm64)/Module.symvers $(kernel_dir_arm64)/scripts \
+	    $(kernel_dir_arm64)/include \
+	    $(kheader_dir_arm64)/usr/src/linux-headers-$(KERNEL_VERSION_ARM64)/
+	mkdir -p $(kheader_dir_arm64)/usr/src/linux-headers-$(KERNEL_VERSION_ARM64)/tools/objtool/
+	mkdir -p $(kheader_dir_arm64)/usr/src/linux-headers-$(KERNEL_VERSION_ARM64)/arch/arm64/
+	cp -r $(kernel_dir_arm64)/arch/arm64/Makefile \
+	    $(kernel_dir_arm64)/arch/arm64/include \
+	    $(kheader_dir_arm64)/usr/src/linux-headers-$(KERNEL_VERSION_ARM64)/arch/arm64
+	cd $(kheader_dir_arm64) && tar cjf $(abspath $@) .
 
-$(kernel_dir)/.config: $(kernel_pardir)/config-$(KERNEL_VERSION)
-	rm -rf $(kernel_dir)
-	wget -O - https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$(KERNEL_VERSION).tar.xz | \
-	    tar xJf - -C $(kernel_pardir)
-	cd $(kernel_dir) && patch -p1 < ../linux-$(KERNEL_VERSION)-timers-gem5.patch
+$(kernel_dir_arm64)/.config: $(kernel_pardir_arm64)/config-$(KERNEL_VERSION_ARM64)
+	rm -rf $(kernel_dir_arm64)
+	wget -O - https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$(KERNEL_VERSION_ARM64).tar.xz | \
+	    tar xJf - -C $(kernel_pardir_arm64)
+	cd $(kernel_dir_arm64) && patch -p1 < ../linux-$(KERNEL_VERSION_ARM64)-timers-gem5.patch
 	cp $< $@
 
 
-CLEAN := 
-DISTCLEAN := $(kernel_dir) $(packer) $(bz_image) $(vmlinux) $(kheader_dir) \
-    $(foreach i,$(IMAGES),$(dir $(i)) $(subst output-,input-,$(dir $(i)))) \
+CLEAN :=
+DISTCLEAN := $(kernel_dir_arm64) $(packer_arm64) $(bz_image_arm64) \
+    $(vmlinux_arm64) $(kheader_dir_arm64) \
+    $(foreach i,$(IMAGES_ARM64),$(dir $(i)) $(subst output-,input-,$(dir $(i)))) \
     $(d)packer_cache $(d)kheaders.tar.bz2
 
 include mk/subdir_post.mk
